4b8b127d9867be7ff59dc8eed390227bd3618d9d
Mehmet TOP <mehmettop91@gmail.com>
2014-04-24 13:16:15 +0300

    Fixed Encoding UTF-8

--- src/CMain.java
+++ src/CMain.java
@@ -1,78 +1,71 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-
-public class CMain {
+public class CMain{
 
 	/**
 	 * @param args
 	 */
-   static List <String>  kisaltma = new ArrayList<String>();
+	static List<String>	kisaltma	= new ArrayList<String>();
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	String cumle = "Ben 2006 yýlýnda Yýldýz Teknik Üniversitesi Bilgisayar Mühendisliðinden" +
-			" mezun oldum. Ayný yýl YTÜ de yüksek lisansa baþladým. Þu an doktoramý" +
-			" bitirmiþ durumdayým. Ýmzamý atarken Dr. Ali Savaþ diye yazýyorum. " +
-			"Doktora sonrasý New York’a gittim. NY üniversitesinden kabul aldým. " +
-			"Bundan sonra TC vatandaþý olduðum halde USA ‘de yaþayacaðým." ;
-	
-	
-	String[] parts = cumle.split(" ");
-	System.out.println("Uzunluk : "+parts.length);
-	int [] isCapital = new int[parts.length];
-	
-    Arrays.fill(isCapital,0);
-    findCapital(parts, isCapital);
-    for(int i=0;i<parts.length;i++){
-    	System.out.println("Kelime : "+parts[i]+" - Kontrol : "+isCapital[i] );
-    }
-    
-   isKisaltma(parts, isCapital);
-   for (String i : kisaltma) {
-	   
-	   System.out.println("Kýsaltma : "+i);
-}
-    
-	}
-	
-public static void findCapital(String[] sentence,int[] index){
-	
-		int i=0;
-		for (String string : sentence) {
-			if(Character.isUpperCase(string.charAt(0))){
-			   	index[i]=1;
-			}
-			
-			i++;
+
+		String cumle = "Ben 2006 yÄ±lÄ±nda YÄ±ldÄ±z Teknik Ãœniversitesi Bilgisayar MÃ¼hendisliÄŸinden" + " mezun oldum. AynÄ± yol YTÃœ de yÃ¼ksek lisansa baÅŸladÄ±m. ÅŸu an doktoramÄ±" + " bitirmiÅŸ durumdayÄ±m. Ä°mzamÄ± atarken Dr. Ali SavaÅŸ diye yazÄ±yorum. " + "Doktora sonrasÄ± New York'a gittim. NY Ãœniversitesinden kabul aldÄ±m. " + "Bundan sonra TC vatandaÅŸÄ± olduÄŸum halde USA'de yaÅŸayacaÄŸÄ±m.";
+
+		String[] parts = cumle.split(" ");
+		System.out.println("Uzunluk : " + parts.length);
+		int[] isCapital = new int[parts.length];
+
+		Arrays.fill(isCapital, 0);
+		findCapital(parts, isCapital);
+		for (int i = 0; i < parts.length; i++) {
+			System.out.println("Kelime : " + parts[i] + " - Kontrol : " + isCapital[i]);
 		}
-	
-		
-		
+
+		isKisaltma(parts, isCapital);
+		for (String i : kisaltma) {
+
+			System.out.println("KÄ±saltma : " + i);
+		}
+
 	}
 
+	public static void findCapital(String[] sentence,int[] index) {
 
-	public static void isKisaltma(String[] sentence,int[] index){
-		
-		int i=0;
-		boolean control=true;
+		int i = 0;
 		for (String string : sentence) {
-			if(index[i]==1){
-				for(int j=0;j<string.length();j++){
-	     			if(!Character.isUpperCase(string.charAt(j))){
-	    			   	control=false;
-	    			}
-	     		}
-	     		if(control){
-	     		kisaltma.add(string);
-	     		}
-	     		control = true;
-				
+			if (Character.isUpperCase(string.charAt(0))) {
+				index[i] = 1;
 			}
-     		
-			
+
 			i++;
 		}
-		
+
+	}
+
+	public static void isKisaltma(String[] sentence,int[] index) {
+
+		int i = 0;
+		boolean control = true;
+		for (String string : sentence) {
+			if (index[i] == 1) {
+				for (int j = 0; j < string.length(); j++) {
+					if (!Character.isUpperCase(string.charAt(j))) {
+						control = false;
+					}
+				}
+				if (control) {
+					kisaltma.add(string);
+				}
+				control = true;
+
+			}
+
+			i++;
+		}
+
 	}
 
 }
